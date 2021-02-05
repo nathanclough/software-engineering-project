15386794711373d3616ef896ce0fd8fae46b28c8
nathanclough <nathanc@betenbough.com>
2021-02-05 10:44:50 -0600

    Created Basic login page with antd package

--- application/app/src/App.js
+++ application/app/src/App.js
@@ -3,15 +3,11 @@ import './App.css';
 import Login from './Components/Login';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <Login/>
-        </p>
-        
-      </header>
+    <div>
+        <img src={logo} className="App-logo" alt="logo" />   
+        <Login/>
     </div>
+
   );
 }
 
