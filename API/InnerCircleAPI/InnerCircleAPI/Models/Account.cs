9ccbfbcbd2c488c7e0b527d9f59fcf224c765b0f
Joseph-McClaine <joseph.mcclaine@ttu.edu>
2021-02-13 19:33:28 -0600

    Added email as its own entitiy

--- API/InnerCircleAPI/InnerCircleAPI/Models/Account.cs
+++ API/InnerCircleAPI/InnerCircleAPI/Models/Account.cs
@@ -12,13 +12,14 @@ namespace InnerCircleAPI.Models
         public long AccountId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Email { get; set; }
+        public virtual Email Email { get; set; }
         public virtual Password Password { get; set; }
         public virtual Username Username { get;set; }
         public Account()
         {
             Password = new Password();
             Username = new Username();
+            Email = new Email();
         }
     }
 }
