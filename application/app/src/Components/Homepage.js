caa13ec9e168b765f94edca0776768da3c488e59
nathanclough <nathanc@betenbough.com>
2021-02-21 20:27:51 -0600

    first draft of the homepage

--- application/app/src/Components/Homepage.js
+++ application/app/src/Components/Homepage.js
@@ -1,18 +1,38 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from "react-router-dom";
+import { Layout, Menu, Space} from 'antd';
+import logo from '../logo.png';
+import '../index.css';
 
 // To do : remove token on signout 
+
+const { Header, Footer, Sider, Content } = Layout;
+
 function Homepage(props){
     var location = useLocation();
     return(
         <>
-        <h1>{location.state.token}</h1>
-        <h2>{location.state.accountID}</h2>
-        <div>
-            <Link to="/">signout</Link>
-        </div>
-        
+    <Layout className="layout">
+    <Header>
+    <img src={logo} className="logo" />
+    <Menu style={{backgroundcolor: 'red'}} theme="light" mode="horizontal" defaultSelectedKeys={['2']}>
+        <Menu.Item key="1">nav 1</Menu.Item>
+        <Menu.Item key="2">nav 2</Menu.Item>
+        <Menu.Item key="3">nav 3</Menu.Item>
+      </Menu>
+    </Header>
+    <Layout style={{padding: '5px 0px'}}>
+    <Sider theme="light" >
+        side bar 
+    </Sider>
+    <Content style={{ padding: '25px 25px' }}>
+      <div className="site-layout-content">Content</div>
+    </Content>
+    </Layout>
+    <Footer className="site-layout-footer"style={{ textAlign: 'center' }}>Inner Circle ©2021 </Footer>
+  </Layout>
         </>
     );
 }
+
 export default Homepage
