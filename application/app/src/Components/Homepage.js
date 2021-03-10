22499b7f43c7ae680b5f9a082ea06037787cc746
nathanclough <nathanc@betenbough.com>
2021-03-09 19:03:35 -0600

    Created UserSearch Component and added it to homepage

--- application/app/src/Components/Homepage.js
+++ application/app/src/Components/Homepage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useLocation,Redirect } from "react-router-dom";
+import Search from './UserSearch'
 import { Layout, Menu, Space} from 'antd';
 import logo from '../logo.png';
 import '../index.css';
@@ -11,32 +12,34 @@ const { Header, Footer, Sider, Content } = Layout;
 function Homepage(props){
     var location = useLocation();
     return(
-        <>
-    <Layout className="layout">
-    <Header>
-    <Menu theme="light" mode="horizontal" defaultSelectedKeys={['1']}>
-        <Link to="/">Sign Out</Link>
-        <img src={logo} className="logo" />
-        <Menu.Item key="1">Home</Menu.Item>
-        <Menu.Item key="2">Circle</Menu.Item>
-        <Menu.Item key="3">Messaging</Menu.Item>
-      </Menu>
-    </Header>
-    <Layout style={{padding: '5px 0px'}}>
-    <Sider theme="light"
-      breakpoint="lg"
-      collapsedWidth="0"
-    >
-        side bar 
-    </Sider>
-    <Content style={{ padding: '25px 40px' }}>
-      <div className="site-layout-content">Content</div>
-    </Content>
-    </Layout>
-    <Footer className="site-layout-footer"style={{ textAlign: 'center' }}>Inner Circle ©2021 </Footer>
-  </Layout>
-  </>
-      );
-}
+        <Layout className="layout">
+          
+          <Header>
+            <Menu theme="light" mode="horizontal" defaultSelectedKeys={['1']}>
+                <Link to="/">Sign Out</Link>
+                <img src={logo} className="logo" />
+                <Menu.Item key="1">Home</Menu.Item>
+                <Menu.Item key="2">Circle</Menu.Item>
+                <Menu.Item key="3">Messaging</Menu.Item>
+              </Menu>
+          </Header>
 
+          <Layout style={{padding: '5px 0px'}}>
+            <Sider theme="light"
+              breakpoint="lg"
+              collapsedWidth="0"
+            >
+              <div className="side-search"><Search/></div>
+            </Sider>
+            
+            <Content style={{ padding: '25px 40px' }}>
+              <div className="site-layout-content">Content</div>
+            </Content>
+          </Layout>
+
+          <Footer className="site-layout-footer"style={{ textAlign: 'center' }}>Inner Circle ©2021 </Footer>
+        
+        </Layout>
+     );
+}
 export default Homepage
