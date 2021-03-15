739d7179484278366b428308d8ee5dc606dc581a
nathanclough <nathanc@betenbough.com>
2021-03-15 12:01:56 -0500

    Added migrations to source

---
+++ API/InnerCircleAPI/InnerCircleAPI/Migrations/20210315144056_UpdateDatabaseWithCircleAndRequests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InnerCircleAPI.Migrations
+{
+    public partial class UpdateDatabaseWithCircleAndRequests : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Requests",
+                columns: table => new
+                {
+                    RequestId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SenderId = table.Column<long>(type: "bigint", nullable: false),
+                    RecepientId = table.Column<long>(type: "bigint", nullable: false),
+                    status = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requests", x => x.RequestId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Requests");
+        }
+    }
+}
